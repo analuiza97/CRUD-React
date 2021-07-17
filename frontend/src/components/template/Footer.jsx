c7c4771700beedcd5eb666ed6b2c85b44dd8b9a7
Ana Luíza Fontes <analuizaffontes97@gmail.com>
2021-07-16 21:10:49 -0300

    Customizando footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
-            <strong> Cod<span className="text-danger">3</span>r</strong>
-        </span>
+        <div> Desenvolvido por {" "}
+            <a href="https://www.instagram.com/analuizaf97/" target="_blank">@analuizaf97</a>
+        </div>
     </footer>
