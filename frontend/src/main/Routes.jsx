d8d1ff7cb47b5682cfec381b7da18ba73480c14c
Ana Luíza Fontes <analuizaffontes97@gmail.com>
2021-07-16 21:17:05 -0300

    Corrigindo rotas

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router'
-
 import Home from '../components/home/Home'
-import UserCrud from '../components/user/UserCrud'
+import Crud from '../components/crud/Crud'
 
 export default props => 
     <Switch>
         <Route exact path='/' component={Home} />
-        <Route path='/users' component={UserCrud} />
+        <Route path='/crud' component={Crud} />
         <Redirect from='*' to='/' />
     </Switch>
