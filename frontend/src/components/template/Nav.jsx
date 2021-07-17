71ee13f90f900f70fade45899d48409f5bd162ee
Ana Luíza Fontes <analuizaffontes97@gmail.com>
2021-07-16 21:15:39 -0300

    Refatorando navbar

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,16 +1,16 @@
-import './Nav.css'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import "./Nav.css";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default props =>
-    <aside className="menu-area">
-        <nav className="menu">
-            {/* Refatorar em casa! */}
-            <Link to="/">
-                <i className="fa fa-home"></i> Início
-            </Link>
-            <Link to="/users">
-                <i className="fa fa-users"></i> Usuários
-            </Link>
-        </nav>
-    </aside>
+export default (props) => (
+  <aside className="menu-area">
+    <nav className="menu">
+      <Link to="/">
+        <i className="fa fa-home"></i> Início
+      </Link>
+      <Link to="/crud">
+        <i className="fa fa-book"></i> Cadastro
+      </Link>
+    </nav>
+  </aside>
+);
